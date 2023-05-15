d84e350ee0bb079d4c98efca00181528f558942a
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-15 12:11:17 -0500

    exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Patrick");
+
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("Enter Something blaaaaah,: ");
+        userInput = sc.nextLine();
+        System.out.println("You entered:" + userInput);
+
+
     }
 }
