9cebbdfab7997cd53c06a9b656d83b6da000fa8e
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-19 16:20:11 -0500

    exercisesjava

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 public class MethodsExercise {
     public static void main(String[] args) {
 
-        int num1 = 10;
-        int num2 = 5;
-
-        int additionResult = addition(num1, num2);
-        int subtractionResult = subtraction(num1, num2);
-        int multiplicationResult = multiplication(num1, num2);
-        double divisionResult = division(num1, num2);
-        int modulusResult = modulus(num1, num2);
+//#1
+      /*  int additionResult = addition(max, min);
+        int subtractionResult = subtraction(max, min);
+        int multiplicationResult = multiplication(max, min);
+        double divisionResult = division(max, min);
+        int modulusResult = modulus(max, min);
 
         System.out.println("Addition Result: " + additionResult);
         System.out.println("Subtraction Result: " + subtractionResult);
@@ -19,81 +17,78 @@ public class MethodsExercise {
         System.out.println("Division Result: " + divisionResult);
         System.out.println("Modulus Result: " + modulusResult);
 
+    }
+*/
+      /*  //#1
+        public static int addition ( int num1, int num2){
+            return num1 + num2;
+        }
+
+        public static int subtraction ( int num1, int num2){
+            return num1 - num2;
+        }
+
+        public static int multiplication ( int num1, int num2){
+            return num1 * num2;
+        }
+
+        public static double division ( int num1, int num2){
+            return (double) num1 / num2;
+        }
+
+        public static int modulus ( int num1, int num2){
+            return num1 % num2;
+        }
+
+    }*/
+
+
+       /* //#2
+        int min = 5;
+        int max = 10;
+        getInteger(min, max);
+    }
+    public static void getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
-        String choice;
+        System.out.println("Enter a number between " + min + " and " + max + ": ");
 
-        do {
+        int userInput = sc.nextInt();
 
-            int number = getInput(scanner);
-            long factorial = calculateFactorial(number);
-            System.out.println("the factorial of" + number + " is: " + factorial);
-            System.out.println("Do you want to continue? (Y/N): ");
+        if (userInput >= min && userInput <= max) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+            getInteger(min, max);
 
-        } while (choice.equalsIgnoreCase("Y"));
+        }
 
-        System.out.println("Program exited.");
-    }
 
-    public static int addition(int num1, int num2) {
-        return num1 + num2;
-    }
+    }*/
 
-    public static int subtraction(int num1, int num2) {
-        return num1 - num2;
-    }
 
-    public static int multiplication(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    public static double division(int num1, int num2) {
-        return (double) num1 / num2;
-    }
-
-    public static int modulus(int num1, int num2) {
-        return num1 % num2;
+        //#3
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
+        System.out.println("Valid input: " + userInput);
     }
 
     public static int getInteger(int min, int max) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         int userInput;
-        do {
-            System.out.println("Enter a number between " + min + " and " + max + ": ");
-            while (!sc.hasNextInt()) {
-                String input = sc.next();
-                System.out.println("Invalid input. Please enter integer...");
-            }
-            userInput = sc.nextInt();
-            while (userInput < min || userInput > max) ;
 
-            return userInput;
-        }
-        public static long calculateFactorial(int number){
-            long factorial = 1;
-            for (int i = 2; i <= number; i++) {
-                factorial *= i;
+        do {
+            while (!scanner.hasNextInt()) {
+                String input = scanner.next();
+                System.out.println("Invalid input. Please enter a valid number.");
             }
-            return (int) factorial;
-        }
+            userInput = scanner.nextInt();
+
+            if (userInput < min || userInput > max) {
+                System.out.println("Input out of range. Please enter a number between " + min + " and " + max + ".");
+            }
+        } while (userInput < min || userInput > max);
+
+        return userInput;
     }
 }
-
-
-
-        /* public static int getInput(Scanner scanner) {
-             int number;
-             do {
-                 System.out.print("Enter an integer from 1 to 10: ");
-                 while (!scanner.hasNextInt()) {
-                     String input = scanner.next();
-                     System.out.println("Invalid input. Please enter an integer.");
-                 }
-                 number = scanner.nextInt();
-             } while (number < 1 || number > 10);
-
-             return number;
-         }*/
-
-
 
