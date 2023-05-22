561245c2f1f5625ecf5aee8f3248a3b49f004cc8
Patrick Mireles <patrickm.mireles@gmail.com>
2023-05-22 14:23:41 -0500

    oojava

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Scanner;
 
 public class HighLow {
@@ -13,3 +14,4 @@ public class HighLow {
             return (int) (Math.random() * dieSides);
     }
 }
+*/
---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+    //return the persons name
+    public String getName() {
+        return name;
+    }
+
+
+    //change the name field to the passed value
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    // print a message to the console using the persons name
+    public void sayHello() {
+        System.out.println("Hello, " + name + "!");
+    }
+    public static void main(String[] args) {
+        Person person = new Person("Patricio M.");
+        System.out.println("Name: " + person.getName());
+        person.setName("Codeup Class");
+        person.sayHello();
+    }
+}
